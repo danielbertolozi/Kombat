29531864695409d167b23b78effb0f8f3c368364
Daniel Bertolozi <danielbertolozi@gmail.com>
2017-10-08 15:51:41 -0300

    Refactor main method

--- KombatClient/src/com/bertolozi/Client/KombatMainForm.java
+++ KombatClient/src/com/bertolozi/Client/KombatMainForm.java
@@ -116,28 +116,15 @@ public class KombatMainForm extends javax.swing.JFrame implements Runnable {
         gameFlow();
     }
 
-    public static void main(String args[]) {
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
+    public static void main(String args[]) throws ClassNotFoundException, InstantiationException,
+                                                  IllegalAccessException, UnsupportedLookAndFeelException {
+        for (UIManager.LookAndFeelInfo info : getInstalledLookAndFeels()) {
+            if ("Nimbus".equals(info.getName())) {
+                javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                break;
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(KombatMainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(KombatMainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(KombatMainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(KombatMainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
 
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new KombatMainForm().setVisible(true);
-            }
-        });
+        invokeLater(() -> new KombatMainForm().setVisible(true));
     }
 }
