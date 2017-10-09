d869483395f5257c805d341c36e6a3aacca96384
Daniel Bertolozi <danielbertolozi@gmail.com>
2017-10-08 22:34:38 -0300

    Create separate method for thread (to avoid making the whole class as a runnable)

--- KombatClient/src/com/bertolozi/Server/Server.java
+++ KombatClient/src/com/bertolozi/Server/Server.java
@@ -3,19 +3,14 @@ package com.bertolozi.Server;
 import com.bertolozi.Player.ServerAgent;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server implements Runnable {
+public class Server {
     public static final int SPEED = 8;
-    public ServerAgent player;
-    boolean btR = false;
-    boolean btL = false;
-    boolean btU = false;
-    boolean btD = false;
-    PrintWriter out;
 
     public static void main(String[] args) {
         System.out.println("Starting...");
@@ -23,80 +18,84 @@ public class Server implements Runnable {
         server.waitForPlayer();
     }
 
-    @Override
-    public void run() {
-        try {
-            while (true) {
-                Thread.sleep(30);
-                if (btR) {
-                    player.x += SPEED;
-                    out.println(player.x + "_" + player.y + "_"
-                            + player.w + "" + player.h);
+    private Runnable getPlayerHandle(ServerAgent player, BufferedReader in, PrintWriter out) {
+        Thread keymapLoop = new Thread(() -> {
+            String command = "";
+            try {
+                while (!(command = in.readLine()).equals("exit")) {
+                    if (command.equals("PR_R")) {
+                        player.moveRight = true;
+                    }
+                    if (command.equals("RE_R")) {
+                        player.moveRight = false;
+                    }
+                    if (command.equals("PR_L")) {
+                        player.moveLeft = true;
+                    }
+                    if (command.equals("RE_L")) {
+                        player.moveLeft = false;
+                    }
+                    if (command.equals("PR_U")) {
+                        player.moveUp = true;
+                    }
+                    if (command.equals("RE_U")) {
+                        player.moveUp = false;
+                    }
+                    if (command.equals("PR_D")) {
+                        player.moveDown = true;
+                    }
+                    if (command.equals("RE_D")) {
+                        player.moveDown = false;
+                    }
                 }
-                if (btL) {
-                    player.x -= SPEED;
-                    out.println(player.x + "_" + player.y + "_"
-                            + player.w + "" + player.h);
-                }
-                if (btL) {
-                    player.x -= SPEED;
-                    out.println(player.x + "_" + player.y + "_"
-                            + player.w + "" + player.h);
-                }
-                if (btD) {
-                    player.y += SPEED;
-                    out.println(player.x + "_" + player.y + "_"
-                            + player.w + "" + player.h);
-                }
-                if (btU) {
-                    player.y -= SPEED;
-                    out.println(player.x + "_" + player.y + "_"
-                            + player.w + "" + player.h);
-                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        });
+
+        return () -> {
+            keymapLoop.start();
+            try {
+                while (true) {
+                    Thread.sleep(30);
+                    player.move();
+                    if (player.moveRight) {
+                        player.x += SPEED;
+                        out.println(player.x + "_" + player.y + "_"
+                                + player.w + "" + player.h);
+                    }
+                    if (player.moveLeft) {
+                        player.x -= SPEED;
+                        out.println(player.x + "_" + player.y + "_"
+                                + player.w + "" + player.h);
+                    }
+                    if (player.moveDown) {
+                        player.y += SPEED;
+                        out.println(player.x + "_" + player.y + "_"
+                                + player.w + "" + player.h);
+                    }
+                    if (player.moveUp) {
+                        player.y -= SPEED;
+                        out.println(player.x + "_" + player.y + "_"
+                                + player.w + "" + player.h);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
     }
 
     public void waitForPlayer() {
         try {
-            player = new ServerAgent();
+            ServerAgent player = new ServerAgent();
             ServerSocket ss = new ServerSocket(8880);
             Socket s = ss.accept();
             BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            out = new PrintWriter(s.getOutputStream(), true);
-
-            Thread th = new Thread(this);
+            PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+            Runnable runnable = getPlayerHandle(player, in, out);
+            Thread th = new Thread(runnable);
             th.start();
-
-            String command = "";
-            while (!(command = in.readLine()).equals("exit")) {
-                if (command.equals("PR_R")) {
-                    btR = true;
-                }
-                if (command.equals("RE_R")) {
-                    btR = false;
-                }
-                if (command.equals("PR_L")) {
-                    btL = true;
-                }
-                if (command.equals("RE_L")) {
-                    btL = false;
-                }
-                if (command.equals("PR_U")) {
-                    btU = true;
-                }
-                if (command.equals("RE_U")) {
-                    btU = false;
-                }
-                if (command.equals("PR_D")) {
-                    btD = true;
-                }
-                if (command.equals("RE_D")) {
-                    btD = false;
-                }
-            }
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(1);
