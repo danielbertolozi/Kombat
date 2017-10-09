725fd0b49d4e29c138437186714fd632765014bd
Daniel Bertolozi <danielbertolozi@gmail.com>
2017-10-08 22:48:47 -0300

    Move getPlayerHandle to Player class

--- KombatClient/src/com/bertolozi/Player/ServerAgent.java
+++ KombatClient/src/com/bertolozi/Player/ServerAgent.java
@@ -1,5 +1,9 @@
 package com.bertolozi.Player;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class ServerAgent {
     public int x = 0;
     public int y = 0;
@@ -9,8 +13,77 @@ public class ServerAgent {
     public boolean moveLeft = false;
     public boolean moveUp = false;
     public boolean moveDown = false;
+    public static final int SPEED = 8;
 
     public void move() {
         return;
+    }
+
+    public Runnable getPlayerHandle(ServerAgent player, BufferedReader in, PrintWriter out) {
+        Thread keymapLoop = new Thread(() -> {
+            String command = "";
+            try {
+                while (!(command = in.readLine()).equals("exit")) {
+                    if (command.equals("PR_R")) {
+                        player.moveRight = true;
+                    }
+                    if (command.equals("RE_R")) {
+                        player.moveRight = false;
+                    }
+                    if (command.equals("PR_L")) {
+                        player.moveLeft = true;
+                    }
+                    if (command.equals("RE_L")) {
+                        player.moveLeft = false;
+                    }
+                    if (command.equals("PR_U")) {
+                        player.moveUp = true;
+                    }
+                    if (command.equals("RE_U")) {
+                        player.moveUp = false;
+                    }
+                    if (command.equals("PR_D")) {
+                        player.moveDown = true;
+                    }
+                    if (command.equals("RE_D")) {
+                        player.moveDown = false;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        return () -> {
+            keymapLoop.start();
+            try {
+                while (true) {
+                    Thread.sleep(30);
+                    player.move();
+                    if (player.moveRight) {
+                        player.x += SPEED;
+                        out.println(player.x + "_" + player.y + "_"
+                                + player.w + "" + player.h);
+                    }
+                    if (player.moveLeft) {
+                        player.x -= SPEED;
+                        out.println(player.x + "_" + player.y + "_"
+                                + player.w + "" + player.h);
+                    }
+                    if (player.moveDown) {
+                        player.y += SPEED;
+                        out.println(player.x + "_" + player.y + "_"
+                                + player.w + "" + player.h);
+                    }
+                    if (player.moveUp) {
+                        player.y -= SPEED;
+                        out.println(player.x + "_" + player.y + "_"
+                                + player.w + "" + player.h);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
     }
 }
--- KombatClient/src/com/bertolozi/Server/Server.java
+++ KombatClient/src/com/bertolozi/Server/Server.java
@@ -10,80 +10,10 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static final int SPEED = 8;
-
     public static void main(String[] args) {
         System.out.println("Starting...");
         Server server = new Server();
         server.waitForPlayer();
-    }
-
-    private Runnable getPlayerHandle(ServerAgent player, BufferedReader in, PrintWriter out) {
-        Thread keymapLoop = new Thread(() -> {
-            String command = "";
-            try {
-                while (!(command = in.readLine()).equals("exit")) {
-                    if (command.equals("PR_R")) {
-                        player.moveRight = true;
-                    }
-                    if (command.equals("RE_R")) {
-                        player.moveRight = false;
-                    }
-                    if (command.equals("PR_L")) {
-                        player.moveLeft = true;
-                    }
-                    if (command.equals("RE_L")) {
-                        player.moveLeft = false;
-                    }
-                    if (command.equals("PR_U")) {
-                        player.moveUp = true;
-                    }
-                    if (command.equals("RE_U")) {
-                        player.moveUp = false;
-                    }
-                    if (command.equals("PR_D")) {
-                        player.moveDown = true;
-                    }
-                    if (command.equals("RE_D")) {
-                        player.moveDown = false;
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-        return () -> {
-            keymapLoop.start();
-            try {
-                while (true) {
-                    Thread.sleep(30);
-                    player.move();
-                    if (player.moveRight) {
-                        player.x += SPEED;
-                        out.println(player.x + "_" + player.y + "_"
-                                + player.w + "" + player.h);
-                    }
-                    if (player.moveLeft) {
-                        player.x -= SPEED;
-                        out.println(player.x + "_" + player.y + "_"
-                                + player.w + "" + player.h);
-                    }
-                    if (player.moveDown) {
-                        player.y += SPEED;
-                        out.println(player.x + "_" + player.y + "_"
-                                + player.w + "" + player.h);
-                    }
-                    if (player.moveUp) {
-                        player.y -= SPEED;
-                        out.println(player.x + "_" + player.y + "_"
-                                + player.w + "" + player.h);
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        };
     }
 
     public void waitForPlayer() {
@@ -93,7 +23,7 @@ public class Server {
             Socket s = ss.accept();
             BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-            Runnable runnable = getPlayerHandle(player, in, out);
+            Runnable runnable = player.getPlayerHandle(player, in, out);
             Thread th = new Thread(runnable);
             th.start();
         } catch (Exception e) {
