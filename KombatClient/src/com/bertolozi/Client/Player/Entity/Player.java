48779026a4d6e753253d67648dfda7ab77b3b2fd
Daniel Bertolozi <bertolozi@Daniels-MacBook-Air.local>
2017-10-23 21:02:11 -0200

    Add basic health properties for client entity

--- KombatClient/src/com/bertolozi/Client/Player/Entity/Player.java
+++ KombatClient/src/com/bertolozi/Client/Player/Entity/Player.java
@@ -3,6 +3,7 @@ package com.bertolozi.Client.Player.Entity;
 public class Player {
     public ScreenElement character = new ScreenElement();
     private int id;
+    private int health;
 
     public Player() {
         this.character.setup();
@@ -14,6 +15,14 @@ public class Player {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
     }
 
     public void move(int x, int y) {
